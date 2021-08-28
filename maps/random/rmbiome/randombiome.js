4aed2e626d29e0c2b7c85e32440a7ec15b2e59a3
Justus Avramenko <michaeldhafer@gmail.com>
2021-08-28 19:00:14 -0400

    Starting fresh from the public files.

--- maps/random/rmbiome/randombiome.js
+++ maps/random/rmbiome/randombiome.js
@@ -14,7 +14,7 @@ function currentBiome()
 function setSelectedBiome()
 {
 	// TODO: Replace ugly default for atlas by a dropdown
-	setBiome(g_MapSettings.Biome || "generic/temperate");
+	setBiome(g_MapSettings.Biome || "generic/alpine");
 }
 
 function setBiome(biomeID)
