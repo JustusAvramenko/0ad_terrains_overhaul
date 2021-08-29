7244021973559a731bc2e85534a6e111f66aefbb
Justus Avramenko <michaeldhafer@gmail.com>
2021-08-29 17:27:21 -0400

    Set default biome to India for testing in Atlas (Atlas really needs a biome feature for rendering random maps as in-game).

--- maps/random/rmbiome/randombiome.js
+++ maps/random/rmbiome/randombiome.js
@@ -14,7 +14,7 @@ function currentBiome()
 function setSelectedBiome()
 {
 	// TODO: Replace ugly default for atlas by a dropdown
-	setBiome(g_MapSettings.Biome || "generic/alpine");
+	setBiome(g_MapSettings.Biome || "generic/india");
 }
 
 function setBiome(biomeID)
